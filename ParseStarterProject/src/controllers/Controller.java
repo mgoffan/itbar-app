297bc1b0ff3502cc9bd95f4f16f5c3d1e23d4aaa
Martin Goffan <martin.goffan@me.com>
2015-05-20 20:35:50 -0300

    preliminary ideas

---
+++ ParseStarterProject/src/controllers/Controller.java
@@ -0,0 +1,11 @@
+package controllers;
+
+/**
+ * Created by martin on 5/20/15.
+ */
+public interface Controller<T> {
+
+    public void save();
+    public void update();
+
+}
--- ParseStarterProject/src/controllers/UserController.java
+++ ParseStarterProject/src/controllers/UserController.java
@@ -1,8 +1,22 @@
 package controllers;
 
+import models.User;
+
 /**
  * Created by martin on 5/20/15.
  */
-public class UserController {
+public class UserController implements Controller<UserController> {
 
+    User user;
+
+
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
 }
