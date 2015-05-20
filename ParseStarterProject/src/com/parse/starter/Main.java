31726d44cf8d2530d1e1a76cbc41f65689e32fba
Martin Goffan <martin.goffan@me.com>
2015-05-20 09:56:20 -0300

    Some Changes

---
+++ ParseStarterProject/src/com/parse/starter/Main.java
@@ -0,0 +1,90 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseUser;
+
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        userUI();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void userUI(){
+        setTitle("ITBAr - " + ParseUser.getCurrentUser().getUsername());
+        TextView menu1 = (TextView) findViewById(R.id.menutitle1txt);
+        TextView menu2 = (TextView) findViewById(R.id.menutitle2txt);
+        TextView menu3 = (TextView) findViewById(R.id.menutitle3txt);
+        TextView menu4 = (TextView) findViewById(R.id.menutitle4txt);
+
+        HorizontalScrollView scrollView = (HorizontalScrollView) findViewById(R.id.scrollView);
+        scrollView.setHorizontalFadingEdgeEnabled(false);
+
+        Button menu1btn = (Button) findViewById(R.id.button);
+        Button menu2btn = (Button) findViewById(R.id.button2);
+        Button menu3btn = (Button) findViewById(R.id.button3);
+        Button menu4btn = (Button) findViewById(R.id.button4);
+
+        menu1.setText("MENU 1");
+        menu2.setText("MENU 2");
+        menu3.setText("MENU 3");
+        menu4.setText("MENU 4");
+
+        pressedButtonMenu(menu1btn);
+        pressedButtonMenu(menu2btn);
+        pressedButtonMenu(menu3btn);
+        pressedButtonMenu(menu4btn);
+
+    }
+
+    public void pressedButtonMenu(Button btn){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ProductDescription.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+
+}
---
+++ ParseStarterProject/src/com/parse/starter/Register.java
@@ -0,0 +1,85 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+
+public class Register extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText legajo = (EditText) findViewById(R.id.legajoregistertxt);
+        final EditText name = (EditText) findViewById(R.id.nametxt);
+        final EditText mail = (EditText) findViewById(R.id.mailtxt);
+        final EditText phone = (EditText) findViewById(R.id.phonetxt);
+        final EditText pass = (EditText) findViewById(R.id.passtxt);
+        final EditText rpass = (EditText) findViewById(R.id.repeatpasstxt);
+        Button register = (Button) findViewById(R.id.registerinregisterbtn);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (pass.getText().toString().equals(rpass.getText().toString()) || !legajo.getText().toString().equals("") || !phone.getText().toString().equals("")){
+                    ParseUser user = new ParseUser();
+                    user.setUsername(legajo.getText().toString());
+                    user.setPassword(pass.getText().toString());
+                    user.setEmail(mail.getText().toString());
+                    user.put("name", name.getText().toString());
+                    user.put("phone",phone.getText().toString());
+                    user.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if(e==null){
+                                Toast.makeText(getApplicationContext(),"Se ha registrado correctamente",Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(getApplicationContext(),"Ha ocurrido un error imprevisto",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }else{
+                    Toast.makeText(getApplicationContext(),"Creo que hiciste algo mal.",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_register, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
